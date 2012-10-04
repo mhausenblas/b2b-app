ae854f36c11235f7adda44ca75f6442fd111629e
Michael Hausenblas <michael.hausenblas@gmail.com>
2012-10-04 18:43:06 +0100

    interlinked Guinness and started enrichment rendering

--- b2b-app.js
+++ b2b-app.js
@@ -7,6 +7,7 @@ var b2b_data = '<http://localhost:2020/person/1> <http://ontologi.es/like#likes>
 <http://localhost:2020/person/2> <http://www.w3.org/2000/01/rdf-schema#label> "Richard Cyganiak" .\
 <http://localhost:2020/person/2> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> .\
 <http://localhost:2020/beer/1> <http://www.w3.org/2000/01/rdf-schema#label> "Guinness" .\
+<http://localhost:2020/beer/1> <http://www.w3.org/2002/07/owl#sameAs> <http://dbpedia.org/resource/Guinness> .\
 <http://localhost:2020/beer/1> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Product> .\
 <http://localhost:2020/beer/2> <http://www.w3.org/2000/01/rdf-schema#label> "Bud" .\
 <http://localhost:2020/beer/2> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Product> .\
@@ -24,13 +25,13 @@ $(function(){
 				// issue SPARQL query against in-memory data
 				store.load("text/turtle", b2b_data, function(success, results) {
 					if(success){
-						store.execute('SELECT * WHERE { ?person <http://ontologi.es/like#likes> ?beer . ?person <http://www.w3.org/2000/01/rdf-schema#label> ?person_label . ?beer <http://www.w3.org/2000/01/rdf-schema#label> ?beer_label . FILTER regex(?person_label, "' + person_name + '", "i") }', function(success, results){
+						store.execute('SELECT * WHERE { ?person <http://ontologi.es/like#likes> ?beer . ?person <http://www.w3.org/2000/01/rdf-schema#label> ?person_label . ?beer <http://www.w3.org/2000/01/rdf-schema#label> ?beer_label . ?beer <http://www.w3.org/2002/07/owl#sameAs> ?alias FILTER regex(?person_label, "' + person_name + '", "i") }', function(success, results){
 							if(success) {
 								for (var i=0; i < results.length; i++) {
 									buf += '<div class="beer">';
 									// buf += '<img src="img/guinness-logo.png" alt="Guinness image" />';
 									buf += '<h3>'+ results[i].beer_label.value + '</h3>';
-									// buf += '<p>Guinness is a popular Irish dry stout that originated in the brewery of Arthur Guinness (1725–1803) at St. James\'s Gate, Dublin. Guinness is one of the most successful beer brands worldwide. It is brewed in almost 60 countries and is available in over 100.[1] 850 million litres (1.5 billion imperial or 1.8 billion US pints) are sold annually.</p>';
+									buf += '<p>' + enrich_beer(store, results[i].alias.value) + '</p>';
 									buf += '</div></div>';
 								}
 								$('#result').html(buf);
@@ -55,4 +56,18 @@ $(function(){
 		});
 	});
 });
+
+
+function enrich_beer(store, beer) {
+	store.execute('LOAD <' + beer + '>', function() {
+		store.execute('SELECT * WHERE { <' + beer +'> <http://dbpedia.org/ontology/abstract> ?abstract .}', function(success, results){
+			if(success) {
+				return results[0].abstract.value;
+			}
+			else {
+				return null;
+			}
+		});
+	});
+}
 
